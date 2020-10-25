5c2febb42dc963ae85f54f24f4d2ee6cefd962ac
Mateusz <mateusz.romek@outlook.com>
2020-10-25 13:21:42 +0100

    Renamed theme folder added modified index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,31 @@
-import React from "react"
-
+import React from 'react'
+import Layout from '../theme/Layout'
+import theme from '../styles/theme'
+import GlobalStyles from '../styles/globalStyle'
+import { Helmet } from 'react-helmet'
+import { ThemeProvider } from 'styled-components'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Helmet>
+      <Layout>
+        <>
+          <Hero />
+          <About />
+          <Projects />
+          <Contact />
+        </>
+      </Layout>
+    </ThemeProvider>
+  )
 }
